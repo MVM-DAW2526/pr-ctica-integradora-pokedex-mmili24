d47a5c98111b59ba7ad22143bff59b77a629b4a8
mmili24 <mmilicevic@institutmvm.cat>
2026-04-10 01:05:00 +0200

    Added files

---
+++ js/pokemon.js
@@ -0,0 +1,14 @@
+class Pokemon {
+  constructor(id, name, types, height, weight, stats, abilities, sprites) {
+    this.id = id;
+    this.nom = name;
+    this.tipus = types;
+    this.altura = height;
+    this.pes = weight;
+    this.stats = stats;
+    this.habilitats = abilities;
+    this.sprites = sprites;
+  }
+
+  static infoApi(data) {
+  } 
