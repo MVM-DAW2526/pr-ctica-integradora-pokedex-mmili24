e6fe0121f60c867ac3134ef635bde391bedadf9d
mmili24 <mmilicevic@institutmvm.cat>
2026-04-11 22:22:15 +0200

    Added search filter

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
+let pokemonList = [];
+
 async function loadPokemons() {
     try {
         const list = await fetchPokemonList();
         const pokemons = await Promise.all(
         list.map(p => fetchPokemon(p.name)));
+        pokemonList = pokemons;
         renderPokemons(pokemons);
     } catch (error) {
         console.error(`❌ Error: ${error.message}`);
@@ -12,6 +15,7 @@ async function loadPokemons() {
 
 function renderPokemons(pokemons) {
     const grid = document.getElementById('mainPkmGrid');
+    grid.replaceChildren();
     pokemons.forEach(pokemon => {
         const card = document.createElement('div');
         const nom = document.createElement('p');
@@ -27,4 +31,12 @@ function renderPokemons(pokemons) {
 
 document.addEventListener('DOMContentLoaded', () => {
     loadPokemons();
+    document.getElementById('pkmSearchInput').addEventListener('input', (event) => {
+        const idOrName = event.target.value.toLowerCase();
+        const filteredPokemons = pokemonList.filter(pokemon => 
+            pokemon.name.toLowerCase().includes(idOrName) || 
+            pokemon.id.toString() === idOrName
+        );
+        renderPokemons(filteredPokemons);
+    });
 });
